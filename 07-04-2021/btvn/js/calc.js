5028b5ab77e8c511986a29841d255ee3c958efcc
Hoang Phuc <trus2lun@gmail.com>
2021-04-11 21:18:37 +0700

    update

--- 07-04-2021/btvn/js/calc.js
+++ 07-04-2021/btvn/js/calc.js
@@ -1,10 +1,11 @@
-var result = '';
+var result = '';	
+var newResult = '';
 function addChar(number) {
 	result += number;
 	document.getElementById('result').innerHTML = result;
 }
 function calcResult() {
-	document.getElementById('result').innerHTML = eval(result);
+	document.getElementById('result').innerHTML = Math.ceil(eval(result) * 100000)/100000;
 }
 function resetCal() {
 	result = '';
