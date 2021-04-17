98dc57f2f225e9f148ed6802ced1e2d7b53c36c4
Hoang Phuc <trus2lun@gmail.com>
2021-04-18 05:58:47 +0700

    update

---
+++ 2021-04-16/js/form_tien_dien.js
@@ -0,0 +1,97 @@
+function TienDien(form) {
+	var name, first_day, last_day, first_number, last_number, gender;
+	var totalNumber = 0;
+	var totalMooney = 0;
+	var printBill = '';
+
+	name = document.getElementById('name').value;
+
+	first_day = document.getElementById('first_day').value;
+
+	last_day = document.getElementById('last_day').value;
+
+	first_number = document.getElementById('first_number').value;
+
+	last_number = document.getElementById('last_number').value;
+
+	date_of_birth = document.getElementById('dateOfBirth').value;
+
+	errorGender == ''
+
+	if (name == '') {
+		document.getElementById('errorName').innerHTML = 'Please input your full name!';
+	} else {
+		document.getElementById('errorName').innerHTML = '';
+	}
+	if (first_day == '') {
+		document.getElementById('errorFirstDate').innerHTML = 'Please input the first day of the period!';
+	} else {
+		document.getElementById('errorFirstDate').innerHTML = '';
+	}
+	if (last_day == '') {
+		document.getElementById('errorLastDate').innerHTML = 'Please input the end of the period!';
+	} else {
+		document.getElementById('errorLastDate').innerHTML = '';
+	}
+	if (date_of_birth == '') {
+		document.getElementById('errorDate').innerHTML = "Please input your birth day";
+	} else {
+		document.getElementById('errorDate').innerHTML = '';
+	}
+	if ((form.gender[0].checked == false) && (form.gender[1].checked == false) && (form.gender[2].checked == false)){
+		document.getElementById('errorGender').innerHTML = "Please choose your gender";
+	} else {
+		document.getElementById('errorGender').innerHTML = ''
+	}
+	if (first_number == '') {
+		document.getElementById('errorFirstNumber').innerHTML = 'Please input electricity numbers at the beginning of the period!';
+	} else {
+		document.getElementById('errorFirstNumber').innerHTML = '';
+	}
+	if (last_number == '') {
+		document.getElementById('errorLastNumber').innerHTML = 'Please input end of period electricity number!';
+	} else {
+		document.getElementById('errorLastNumber').innerHTML = '';
+	}
+	if (first_day > last_day) {
+		document.getElementById('errorFirstDate').innerHTML = 'The first day of the period must not be greater than the end of the period!';
+	}
+	totalNumber = Number(last_number) - Number(first_number);
+	if (totalNumber <= 100) {
+		totalMoney = totalNumber * 1500;
+	}
+	if (totalNumber > 100 && totalNumber <= 200) {
+		totalMoney = 100 * 1500 + (totalNumber - 100) * 2000;
+	}
+	if (totalNumber > 200) {
+		totalMoney = 100 * 1500 + 100 * 2000 + (totalNumber - 200) * 3500;
+	}
+	printBill += "<p>Ho ten: " + name + "</p>";
+	printBill += "<p>So dau ky: " + first_number + "</p>";
+	printBill += "<p>So cuoi ky: " + last_number + "</p>";
+	printBill += "<p>Ngay cuoi ky: " + first_day + "</p>";
+	printBill += "<p>Ngay cuoi ky: " + last_day + "</p>";
+	printBill += "<p>Tong tien: " + totalMoney + "</p>";
+	if (form.gender[0].checked == true) {
+		document.getElementById('printBill').innerHTML = printBill;
+		document.getElementById('printBill').style.background = '#24F927';
+		document.getElementById('printBill').style.color = 'black';
+		document.getElementById('printBill').style.height = '260px';
+		document.getElementById('printBill').style.width = '200px';
+		document.getElementById('printBill').style.textAlign = "center";
+	}
+	if (form.gender[1].checked == true) {
+		document.getElementById('printBill').innerHTML = printBill;
+		document.getElementById('printBill').style.background = '#FF5DF0';
+		document.getElementById('printBill').style.color = 'black';
+		document.getElementById('printBill').style.height = '260px';
+		document.getElementById('printBill').style.width = '200px';
+	}
+	if (form.gender[2].checked == true) {
+		document.getElementById('printBill').innerHTML = printBill;
+		document.getElementById('printBill').style.background = '#000000';
+		document.getElementById('printBill').style.color = 'white';
+		document.getElementById('printBill').style.height = '260px';
+		document.getElementById('printBill').style.width = '200px';
+	}
+}
